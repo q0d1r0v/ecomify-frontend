00f8a1ba1874ac7ac94ae88971d548824b4e7f0b
q0d1r0v <qodirovoybekjon08@gmail.com>
2024-06-25 00:52:10 +0500

    w-code

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -5,12 +5,13 @@ import { ref, onMounted } from "vue"
 import { useRoute } from "vue-router"
 import { toast } from "vue3-toastify";
 import { client } from "../utils/axios";
+import { CreateUrl } from "../modules/create-url";
 
 // route
 const route = useRoute()
 
 // data
-const product_data = ref(null)
+const product_data = ref<any>(null)
 
 // functions
 async function getProductData() {
@@ -38,11 +39,22 @@ onMounted(() => {
 
 <template>
     <div class="h-screen">
-        <div>
-            Product id {{ $route.query?.product_id }}
+        <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 max-[630px]:grid-cols-1 gap-4 mt-4">
+            <div class="border">
+                <div>
+                    <img :src="CreateUrl(product_data?.images[0].name)" alt="#product_image" width="400">
+                </div>
+                <div v-if="product_data?.images.length > 1">
+                    other images
+                </div>
+            </div>
+            <div class="border">
+                price and other info
+            </div>
         </div>
-        <pre>
-            {{ product_data }}
-        </pre>
+
+        <div class="border mt-4">
+            html information
+        </div>
     </div>
 </template>
